fc21347168f667cfb3e5e2ebd35e953e7b66d1e6
Rohit Kaundal <kaundal.rohit@gmail.com>
2016-01-27 00:15:41 +0530

    db settings updated

--- portal/app/config/database.php
+++ portal/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'yallacal_maindb',
+			'username'  => 'yallacal_admin',
+			'password'  => 'WK)RoHBdCgxI',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
